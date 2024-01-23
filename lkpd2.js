163bf252411ffb84546cbd60c38eee5af54360de
kafkaarko <kafkaarko@gmail.com>
2024-01-23 10:54:11 +0700

    ini lkpd2

---
+++ lkpd2.js
@@ -0,0 +1,34 @@
+let nama = "kafka";
+let gaji_pokok = 10000;
+let pajak;
+let tunjangan;
+let gajiBersih;
+
+tunjangan = 20/100 * gaji_pokok;
+pajak = 15/100 * (gaji_pokok  + tunjangan) ;
+gajiBersih = gaji_pokok + tunjangan - pajak;
+
+console.log(nama)
+console.log(gaji_pokok)
+console.log(tunjangan)
+console.log(pajak)
+console.log(gajiBersih)
+
+
+
+let jam;
+let menit;
+let detik = prompt("detik");
+let hasil;
+let cek;
+
+// jam *= 3600;
+// menit *= 60;
+// hasil = (jam + menit + detik);
+// console.log(hasil);
+cek = detik % 3600;
+jam = detik / 3600;
+menit = cek /60;
+detik = cek %  60;
+
+document.write(Math.floor(jam) + ' Jam ' + Math.floor(menit) + ' Menit ' + Math.floor(detik) + ' Detik ') 
